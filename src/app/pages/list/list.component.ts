6830aae050865820346276d0614e601ecf5f6f72
Razvan Drebitca <programmerplayer@protonmail.com>
2023-02-18 16:15:31 +0200

    worked on frontend changes

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from 'src/app/services/cart.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonList, MenuController, ModalController, NavController } from '@ionic/angular';
+import { Cart, DataService } from 'src/app/services/data.service';
+import { FunctionsService } from 'src/app/services/functions.service';
 
 @Component({
   selector: 'app-list',
@@ -7,35 +9,97 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  cartItems = 0;
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build',
-  ];
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  @ViewChild('slidingList') slidingList: IonList;
 
-  constructor(public cart: CartService) {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)],
-      });
-    }
-    this.cart.getCartTotal().subscribe((val) => {
-      this.cartItems = val;
-    });
+  customAlertOptions: any = {
+    header: 'Select Quantity',
+    translucent: true
+  };
+
+  qty = [];
+  code = '';
+  show = true;
+  data: Array<Cart> = [];
+
+  constructor(
+    private menuCtrl: MenuController,
+    public dataService: DataService,
+    public fun: FunctionsService,
+    private modalController: ModalController,
+    private nav: NavController,
+    public alertController: AlertController) {
+    this.data = dataService.cart;
+    if (this.data.length === 0) { this.show = false; }
+    for (let i = 1; i <= 36; i++) { this.qty.push(i); }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.menuCtrl.enable(true, 'start');
+    this.menuCtrl.enable(false, 'end');
+  }
+
+  async open_modal(b) {
+    let modal;
+    if (b) {
+      modal = await this.modalController.create({
+        component: '',
+        componentProps: { value: this.dataService.terms_of_use, title: 'Terms of Use' }
+      });
+    } else {
+      modal = await this.modalController.create({
+        component: '',
+        componentProps: { value: this.dataService.privacy_policy, title: 'Privacy Policy' }
+      });
+    }
+    return await modal.present();
+  }
+
+  calculate(i) {
+    let res = 0;
+    if (i === 0) {
+      for (const j of this.data) {
+        if (j.product.offer) {
+          res += this.fun.calculate(j.product.cost_price, j.product.discount) * j.quantity;
+        } else {
+          res += j.product.cost_price * j.quantity;
+        }
+      }
+    }
+    if (i === 1) {
+      for (const j of this.data) {
+        res += j.product.shipping;
+      }
+    }
+    return res;
+  }
+
+
+  fix(a) {
+    return a.toFixed(2);
+  }
+
+  add() {
+    const res = this.calculate(1) + this.calculate(0);
+    return res;
+  }
+
+  browse() {
+    this.fun.navigate('/home', false);
+  }
+
+  async remove(j) {
+    this.fun.removeConform().then(res => {
+      console.log('res conform', res);
+      if (res === 'ok') {
+        this.slidingList.closeSlidingItems();
+        this.data.splice(j, 1);
+        if (this.data.length === 0) {
+          this.show = !this.show;
+        }
+      }
+    });
+  }
 }
